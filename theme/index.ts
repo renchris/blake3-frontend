5d1a7f7c946782e010b5b2804d2986e2d68471e9
renchris <57972348+renchris@users.noreply.github.com>
2022-11-19 23:03:12 -0800

    Initial commit

---
+++ theme/index.ts
@@ -0,0 +1,45 @@
+import { extendTheme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+import components from './components'
+
+const fonts = {
+  heading: 'DM Sans, sans-serif',
+  body: 'Poppins, sans-serif',
+}
+
+const breakpoints = createBreakpoints({
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
+})
+
+const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      text: {
+        default: '#16161D',
+        _dark: '#ade3b8',
+      },
+      heroGradientStart: {
+        default: '#7928CA',
+        _dark: '#e3a7f9',
+      },
+      heroGradientEnd: {
+        default: '#FF0080',
+        _dark: '#fbec8f',
+      },
+    },
+    radii: {
+      button: '12px',
+    },
+  },
+  colors: {
+    black: '#16161D',
+  },
+  fonts,
+  breakpoints,
+  components,
+})
+
+export default theme
